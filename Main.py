28358a5c9d25f1126a867a8505b9fbb6eacc4ff2
Avi <avi.turner111@gmail.com>
2017-04-21 16:15:16 +0300

    follow up commit

--- Main.py
+++ Main.py
@@ -4,25 +4,32 @@ from DataVisualization.Visualyzer import Visualyzer
 
 CSV_NAME = "Data.csv"
 from LogisticRegression.LogisticClassifier import LogisticClassifier as lc
+from LogisticRegression.LogisticClassifier_coursera import LogisticClassifier_coursera as lc_coursera
+
 def main():
-    path = os.path.join(os.path.curdir,CSV_NAME)
     logc = lc()
+    run_classifier(logc)
+
+    logc_coursera = lc_coursera()
+    run_classifier(logc_coursera)
+
+    str()
+
+
+def run_classifier(logc):
+    path = os.path.join(os.path.curdir, CSV_NAME)
     logc.load_data_from_csv(path)
     # Visualyzer.display_heat_map(logc.normalized_data, logc.ys)
-
     model = logc.train(training_set_size_percentage=0.528)
     sliced_data = logc.slice_data(training_set_size_percentage=0.528)
-
     test_set = logc.data[300:, :]
     test_y = logc.ys[300:, :]
     ys = logc.classify(test_set)
-
     diffs = ys == test_y
     import numpy as np
     diff_len = len(diffs) - np.count_nonzero(diffs)
-    assert  diff_len == 8, "Expected to have a difference of 8... (was {0})".format(diff_len)
+    # assert diff_len == 8, "Expected to have a difference of 8... (was {0})".format(diff_len)
 
-    str()
 
 if __name__ == "__main__":
     main()
