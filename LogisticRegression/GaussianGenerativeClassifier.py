794b3d268229f87134a2d91f37ffc46f08ebfc07
Avi <avi.turner111@gmail.com>
2017-04-25 21:14:03 +0300

    work on gda

--- LogisticRegression/GaussianGenerativeClassifier.py
+++ LogisticRegression/GaussianGenerativeClassifier.py
@@ -17,20 +17,35 @@ class GaussianGenerativeClassifier(AbstractClassifier):
     def _train(self, t_samples, t_y):
 
 
-        #   m : array_like
-        #        A 1-D selor 2-D array containing multiple variables and observations.
-        #        Each row of `m` represents a variable, and each column a single
-        #        observation of all those variables. Also see `rowvar` below.
-        m = t_samples.transpose()
+        # #   m : array_like
+        # #        A 1-D selor 2-D array containing multiple variables and observations.
+        # #        Each row of `m` represents a variable, and each column a single
+        # #        observation of all those variables. Also see `rowvar` below.
+        # m = t_samples.transpose()
+        #
+        # cov = np.cov(m)
+        # cov_det = np.linalg.det(cov)
+        #
+        # model_prefix = 1.0/(np.sqrt(2*np.pi)*np.power(cov_det,0.5))
+        y_list = list(np.array(t_y).reshape(-1,))
+        all_classes = np.unique(y_list)
 
-        cov = np.cov(m)
-        cov_det = np.linalg.det(cov)
+        #
+        average_by_class = dict((cls,np.mean(t_samples[t_y == cls])) for cls in all_classes)
+        average_by_class = defaultdict(lambda :None, average_by_class)
+        m = len(t_y)
+        one_over_m = 1.0/m
+        def internal_predict(x):
+            diffs = [x - avg for cls, avg in average_by_class]
+            prod_diffs = np.prod(diffs)
+            sum_prod_diffs = np.sum(prod_diffs)
+            prediction = one_over_m * sum_prod_diffs
+            return prediction
 
-        model_prefix = 1.0/(np.sqrt(2*np.pi)*np.power(cov_det,0.5))
-        exp_power = (-0.5)*np.power(((data - avg)/std),2)
-        exp = np.exp(exp_power)
 
-        return  cov
+
+
+        return  internal_predict()
         # all_classes = set(t_y)
         # models = defaultdict(lambda: None)
         # for cls in all_classes:
@@ -42,7 +57,9 @@ class GaussianGenerativeClassifier(AbstractClassifier):
         # return models
 
     def _predict(self, normed_data):
-        raise NotImplementedError("see page 16")
+        internal_predict_func = self._model
+        predictions = [internal_predict_func(features) for features in normed_data ]
+        return predictions
 
     # def __build_model(self, data):
     #     """
