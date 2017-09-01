cab32905432350e1fa38c26f2a9c87b1b9a1cde7
Avi Turner <avi.turner111@gmail.com>
2017-09-01 09:21:55 +0300

    Added data loader for final project data

--- LogisticRegression/DataLoaders/Utils.py
+++ LogisticRegression/DataLoaders/Utils.py
@@ -1,7 +1,9 @@
 from LogisticRegression.DataLoaders.CancerDataLoader import CancerDataLoader
 from LogisticRegression.DataLoaders.CreditDataLoader import CreditDataLoader
+from LogisticRegression.DataLoaders.final_project_data_loader import FinalProjectDataLoader
 
 
 def get_default_data_loader():
+    return FinalProjectDataLoader()
     return CreditDataLoader()
     return CancerDataLoader()
